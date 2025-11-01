ddeff57bc5ffbb1df965d26b35e9fa0e02c1025f
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-01 20:11:49 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:11:41.157Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-01T17:11:49.682Z
+Commit: Fix broken links in documentation
