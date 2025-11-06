7f2e64b03d28236282981a4e375ad2d512feff6e
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:53:31 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:53:27.368Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:53:30.110Z
+Commit: Fix broken links in documentation
