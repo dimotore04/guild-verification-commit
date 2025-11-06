612c6a2d3209a67424c190ff31886c50dfbce859
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:53:44 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:53:21.926Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T01:53:43.273Z
+Commit: Add export functionality for reports
