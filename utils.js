58329727a208f16d1392470ca90b298be2515823
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:57:07 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:57:00.754Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:57:06.095Z
+Commit: Correct typo in configuration file
