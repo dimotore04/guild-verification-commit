7673445c4b3fb9c6c6b84800d9f890fbfa200637
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 05:03:45 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:03:39.972Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:03:44.204Z
+Commit: Fix memory leak in event handlers
