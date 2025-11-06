63d319a8d0b77b531d29a6d0d2af659864883217
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:59:30 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:59:16.193Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-06T01:59:29.074Z
+Commit: Patch security vulnerability in authentication
