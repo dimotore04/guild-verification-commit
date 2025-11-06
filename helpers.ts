af933411edfbffa9b647153f3c8f868ea6f213fb
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 05:02:53 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:02:51.617Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T02:02:52.909Z
+Commit: Fix database connection timeout
