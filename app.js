437ef776dbb8100b16284e67ac5d962840fcff2b
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:59:01 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:58:57.845Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:59:00.080Z
+Commit: Fix CSS styling bug on mobile devices
