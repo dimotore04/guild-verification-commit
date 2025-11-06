2c49fc128ffc11119aff3155ee33507f9304d471
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 05:00:14 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:00:10.431Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:00:12.954Z
+Commit: Fix broken links in documentation
