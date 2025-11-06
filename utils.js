3b165bc385fb9a02040efe516f91321cc160ed1d
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:53:28 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:53:25.660Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T01:53:27.368Z
+Commit: Fix memory leak in event handlers
