b5491e309a8ba108fb415f0135adb5104978ae51
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:53:45 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:12:10.212Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:53:44.561Z
+Commit: Resolve timezone issues in date handling
