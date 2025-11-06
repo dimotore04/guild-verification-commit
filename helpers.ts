2ed6e1ad1c84f35a691d3f91b30ef16672134e80
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 05:03:48 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:03:22.693Z
-Commit: Add dark mode support
+Timestamp: 2025-11-06T02:03:46.927Z
+Commit: Patch security vulnerability in authentication
