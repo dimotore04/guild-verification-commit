79030e9ccefa8c375e2a6a03513d0096f13932b3
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 05:02:57 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:01:33.498Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T02:02:56.847Z
+Commit: Fix CSS styling bug on mobile devices
