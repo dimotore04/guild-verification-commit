a1f9af9d5512417010fab0ce539944243f7975dc
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:57:22 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:53:47.966Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T01:57:20.935Z
+Commit: Add export functionality for reports
