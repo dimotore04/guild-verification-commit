53985066e735c51b1e43cd68a162a443a70a46ca
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:53:49 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:53:43.273Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T01:53:47.966Z
+Commit: Patch security vulnerability in authentication
