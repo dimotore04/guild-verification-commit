090a6b37d2d8b145ff0d13455116d793ad949daa
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:56:36 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:56:32.154Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:56:34.968Z
+Commit: Resolve null pointer exception in user service
