970f40ab72f5c95c233ba5c88e9f126f4989c8e9
nitoss <113218243+dimotore04@users.noreply.github.com>
2025-11-06 04:57:41 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:57:31.950Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:57:40.863Z
+Commit: Patch API endpoint error handling
